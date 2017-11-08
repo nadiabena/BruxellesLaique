936b0b5227eb8e4f31114c85f6ccd1c40bf5995e
nadiabena <nadia.benazouz@gmail.com>
2017-11-08 16:14:45 +0100

    test merge

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,7 +18,9 @@ class CreateUsersTable extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->string('password');
+            /*TEST*/
             $table->rememberToken();
+
             $table->timestamps();
         });
     }
